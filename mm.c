4e58b6e8d4ef0384329e3f110e1153781967c829
Hyein Kim <kimnieyh@gmail.com>
2024-02-15 06:49:34 +0000

    init , coalesce

--- mm.c
+++ mm.c
@@ -44,12 +44,75 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE   4
+#define DSIZE   8
+#define CHUNKSIZE (1<<12) //4096
+#define PACK(size,alloc)    ((size) | (alloc))
+#define PUT(p,val) (*(unsigned int *)(p) = (val))
+#define GET(p)  (*(unsigned int *)(p))
+#define GET_SIZE(p) (GET(p) & ~0x7) // 뒤에 3개는 빼고! 0000111
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define HDRP(bp)    ((char *)(bp)-WSIZE) //헤더 포인터
+#define FTRP(bp)    ((char *)(bp)+GET_SIZE(HDRP(bp)-DSIZE)) //푸터 포인터
+
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
+#define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp)-WSIZE)))
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    void *heap_listp;
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp,0);
+    PUT(heap_listp + (1*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp + (2*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp + (3*WSIZE),PACK(0,1));
+    heap_listp += (2*WSIZE);
+
+    if(extend_heap(CHUNKSIZE/WSIZE)==NULL)
+        return -1;
     return 0;
+}
+static void *coalesce(void *bp){
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if(prev_alloc && next_alloc) {
+        return bp;
+    }else if(prev_alloc && !next_alloc){
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp),PACK(size,0));
+        PUT(FTRP(bp),PACK(size,0));
+    }else if(!prev_alloc && next_alloc){
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp),PACK(size,0));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }else{
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    
+    PUT(HDRP(bp),PACK(size,0));
+    PUT(FTRP(bp),PACK(size,0));
+    put(HDRP(NEXT_BLKP(bp)),PACK(0,1));
+
+    return coalesce(bp);
 }
 
 /* 
