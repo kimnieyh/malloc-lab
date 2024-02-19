58dd604a00304489a5cce3db32ae4bc2338f181b
Hyein Kim <kimnieyh@gmail.com>
2024-02-19 02:24:56 +0000

    fisrst fit 명시적 가용리스트 !

--- mm.c
+++ mm.c
@@ -57,8 +57,8 @@ team_t team = {
 #define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
 //이전블록의 포인터
 #define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp)-DSIZE)))
-#define GET_NEXT(bp) (*(void **)((char *)(bp) + WSIZE)) // 다음 가용 블록의 주소
-#define GET_PREV(bp) (*(void **)(bp))                   // 이전 가용 블록의 주소
+#define GET_NEXT(bp)        (*(void **)((bp) + WSIZE)) // 다음 가용 블록의 주소
+#define GET_PREV(bp)        (*(void **)(bp))                  // 이전 가용 블록의 주소
 
 static char *heap_listp = NULL; // prologue 블록을 가리킴
 static char *free_listp = NULL; //free list 첫 블록 가리킴
@@ -80,32 +80,27 @@ ________________________________________
 //힙 생성
 int mm_init(void)
 {
-
     //초기 힙 생성
-    if((heap_listp = mem_sbrk(8*WSIZE)) == (void *)-1)
+    if((heap_listp = mem_sbrk(6*WSIZE)) == (void *)-1)
         return -1;
     //패딩값 더블 워드 경계로 정렬된 미사용 패딩
-    PUT(heap_listp,0);
-    // 프롤로그 header 
-    PUT(heap_listp + (1*WSIZE),PACK(MINIMUM,1));
-    //prev
-    PUT(heap_listp + (2*WSIZE),NULL);
-    //next
-    PUT(heap_listp + (3*WSIZE),NULL);
-    // free list footer
-    PUT(heap_listp + (4*WSIZE),PACK(MINIMUM,0));
-    //에필로그 헤더 
-    PUT(heap_listp + (5*WSIZE),PACK(0,1));
-    
-    heap_listp += MINIMUM;
+    PUT(heap_listp, 0); //alignment padding
+    PUT(heap_listp + (1*WSIZE), PACK(MINIMUM, 1));
+    PUT(heap_listp + (2*WSIZE), NULL); // prec
+    PUT(heap_listp + (3*WSIZE), NULL); // succ
+    PUT(heap_listp + (4*WSIZE), PACK(MINIMUM, 1));
+    PUT(heap_listp + (5*WSIZE), PACK(0, 1));
+
+    free_listp = heap_listp + 2*WSIZE;
 
     if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
+    {
         return -1;
+    }
     return 0;
 }
  void *mm_malloc(size_t size)
 {   
-    printf("\n mm_malloc start \n");
     //조정된 사이즈
     size_t asize;
     //확장할 사이즈
@@ -146,18 +141,18 @@ static void *find_fit(size_t asize){
 
 static void place(void *bp, size_t asize){
     size_t csize = GET_SIZE(HDRP(bp));
-
     free_delete(bp);
-
-    PUT(HDRP(bp),PACK(asize,1));
-    PUT(FTRP(bp),PACK(asize,1));
     
     if((csize - asize) >= MINIMUM){ 
+        PUT(HDRP(bp),PACK(asize,1));
+        PUT(FTRP(bp),PACK(asize,1));
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp),PACK(csize-asize,0));
         PUT(FTRP(bp),PACK(csize-asize,0));
-        
         free_insert(bp);
+    }else{
+        PUT(HDRP(bp),PACK(asize,1));
+        PUT(FTRP(bp),PACK(asize,1));
     }
 }
 static void free_insert(void *bp){
@@ -175,24 +170,27 @@ static void free_delete(void *bp){
         GET_NEXT(GET_PREV(bp)) = GET_NEXT(bp);
         GET_PREV(GET_NEXT(bp)) = GET_PREV(bp);
     }    
-}
+} 
 
-static void *extend_heap(size_t words){
+static void* extend_heap(size_t words) {
+    // 워드 단위로 받는다.
     char* bp;
     size_t size;
-    
-    size = (words %2) ? (words +1) *WSIZE : (words) * WSIZE;
-    if((long)(bp = mem_sbrk(size))== -1)
-        return NULL;
 
-    PUT(HDRP(bp),PACK(size,0));
-    PUT(FTRP(bp),PACK(size,0));
-    PUT(HDRP(NEXT_BLKP(bp)),PACK(0,1));
+    /* 더블 워드 정렬에 따라 메모리를 mem_sbrk 함수를 이용해 할당받음. mem_sbrk는 힙 용량을 늘려줌.*/ 
+    size = (words % 2) ? (words + 1) * WSIZE : (words) * WSIZE; // size를 짝수 word && byte 형태로 만든다.
+    if ((long)(bp = mem_sbrk(size)) == -1) {// 새 메모리의 첫 부분을 bp로 둔다. 주소값은 int로는 못 받으니 long으로 type casting
+    return NULL;
+    }
+    /* 새 가용 블록의 header와 footer를 정해주고 epliogue block을 가용 블록 맨 끝으로 옮긴다. */
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 새 에필로그 헤더
 
+    /* 만약 이전 블록이 가용 블록이라면 연결 */
     return coalesce(bp);
 }
 
-//todo free  size 가 다른 경우 분기 처리??? 
 void mm_free(void *bp)
 {
     size_t size = GET_SIZE(HDRP(bp));
@@ -203,33 +201,45 @@ void mm_free(void *bp)
 
 //병합 하기 
 static void *coalesce(void *bp){
-    // 이전 블록의 할당 상태
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    // 다음 블록의 할당 상태
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    // 현재 블록 사이즈 
+    //직전 블록의 footer, 직후 블록의 header를 보고 가용 여부를 확인.
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))); // 직전 블록 가용 여부 확인
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))); // 직후 블록 가용 여부 확인
     size_t size = GET_SIZE(HDRP(bp));
 
-    if(prev_alloc && !next_alloc){ //다음 블록만 빈 경우 --> 연결 필요 없음!!
-        free_delete(NEXT_BLKP(bp));
+
+    // case 1: 직전, 직후 블록이 모두 할당 -> 해당 블록만 free list에 넣어준다.
+
+    // case 2: 직전 블록 할당, 직후 블록 가용
+
+    if (prev_alloc && !next_alloc) {
+        free_delete(NEXT_BLKP(bp)); // free 상태였던 직후 블록을 free list에서 제거
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-    } else if(!prev_alloc && next_alloc){ // 이전 빈 경우
-        free_delete(PREV_BLKP(bp));
+    }
+
+    // case 3: 직전 블록 가용, 직후 블록 할당
+    else if (!prev_alloc && next_alloc){ // alloc이 0이니까  !prev_alloc == True.
+        free_delete(PREV_BLKP(bp)); // 직전 블록을 free list에서 제거
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        bp = PREV_BLKP(bp);
-        PUT(HDRP(bp),PACK(size,0));
-        PUT(FTRP(bp),PACK(size,0));
-    } else { // 이전, 다음 모두 빈 경우
-        free_delete(PREV_BLKP(bp));
-        free_delete(PREV_BLKP(bp));
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         bp = PREV_BLKP(bp);
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
     }
+
+    // case 4: 직전, 직후 블록 모두 가용
+    else if (!prev_alloc && !next_alloc) {
+        free_delete(PREV_BLKP(bp));
+        free_delete(NEXT_BLKP(bp));
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp))); // 앞뒤 free된 블록 전체 사이즈 계산
+        bp = PREV_BLKP(bp); // 현재 bp를 가용인 앞쪽 블록으로 이동시켜 => 거기가 전체 free 블록의 앞쪽이 되어야 함.
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    // 연결된 새 가용 블록을 free list에 추가
     free_insert(bp);
+
     return bp;
 }
 
@@ -240,12 +250,11 @@ void *mm_realloc(void *ptr, size_t size)
     if(ptr == NULL) //포인터가 null이면 할당만 수행
         return mm_malloc(size);
     
-    if(size<= 0) //사이즈가 0이면 메모리 반환만 수행
+    if(size == 0) //사이즈가 0이면 메모리 반환만 수행
     {
         mm_free(ptr);
         return 0;
     }
-
     // 새 블록에 할당
     void *newptr = mm_malloc(size);
     if (newptr == NULL)
